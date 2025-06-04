028bdf37c597a0c98d0f71b1dbef3f0880b4fff2
Fabien Lelaquais <fabien.lelaquais@avaiga.com>
2025-06-04 17:10:18 +0200

    Tests

--- taipy/templates/sdm/{{cookiecutter.__root_folder}}/{{cookiecutter.__main_file}}.py
+++ taipy/templates/sdm/{{cookiecutter.__root_folder}}/{{cookiecutter.__main_file}}.py
@@ -9,12 +9,12 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+from config.config import configure
+from pages import job_page, scenario_page
+from pages.root import content, root, selected_data_node, selected_scenario
+
 import taipy as tp
 from taipy import Gui, Orchestrator
-
-from .config.config import configure
-from .pages import job_page, scenario_page
-from .pages.root import content, root, selected_data_node, selected_scenario
 
 
 def on_init(state): ...
