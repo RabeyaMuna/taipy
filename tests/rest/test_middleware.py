e56badcacbfa6003c45430e8b618073519844b07
Fabien Lelaquais <fabien.lelaquais@avaiga.com>
2025-06-04 16:40:25 +0200

    Propagate refactor

--- tests/rest/test_middleware.py
+++ tests/rest/test_middleware.py
@@ -23,7 +23,7 @@ def mock_enterprise_middleware(f):
     return wrapper
 
 
-@patch("taipy.common._check_dependencies.EnterpriseEdition._is_installed")
+@patch("taipy.common._modules.EnterpriseEdition._is_installed")
 @patch("taipy.rest.api.middlewares._middleware._enterprise_middleware")
 def test_enterprise_middleware_applied_when_enterprise_is_installed(
     enterprise_middleware: MagicMock, is_enterprise_installed: MagicMock
@@ -41,7 +41,7 @@ def test_enterprise_middleware_applied_when_enterprise_is_installed(
     enterprise_middleware.assert_called_once()
 
 
-@patch("taipy.common._check_dependencies.EnterpriseEdition._is_installed")
+@patch("taipy.common._modules.EnterpriseEdition._is_installed")
 @patch("taipy.rest.api.middlewares._middleware._enterprise_middleware")
 def test_enterprise_middleware_not_applied_when_enterprise_is_not_installed(
     enterprise_middleware: MagicMock, is_enterprise_installed: MagicMock
