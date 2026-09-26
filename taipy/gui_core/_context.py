0b86782a2b84a580f18786198fb4bcd984fc7366
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 22:47:55 -0400

    RabeyaMuna__diff__id_141

--- taipy/gui_core/_context.py
+++ taipy/gui_core/_context.py
@@ -663,9 +663,8 @@ class _GuiCoreContext(_CoreEventConsumerBase):
             filtered_list = [
                 e
                 if isinstance(e, DataNode)
-                else self.filter_entities(d, t.cast(str, col), col_type, False, action, val, col_fn)
+                else self.filter_entities(t.cast(list, e), t.cast(str, col), col_type, False, action, val, col_fn)
                 for e in filtered_list
-                for d in (t.cast(list, t.cast(list, e)[2]) if isinstance(e, list) else [e])
             ]
         # remove empty cycles
         return [e for e in filtered_list if isinstance(e, DataNode) or (isinstance(e, (tuple, list)) and len(e[2]))]
